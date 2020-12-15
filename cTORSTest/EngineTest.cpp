bf61c3a77684c97516d8d1c8ab4b6fd8458fe433
Koos van der Linden <J.G.M.vanderLinden@tudelft.nl>
2020-12-15 18:01:05 +0100

    Scenario test case

--- cTORSTest/EngineTest.cpp
+++ cTORSTest/EngineTest.cpp
@@ -35,4 +35,34 @@ namespace cTORSTest
 			engine.EndSession(state);
 		}		
 	}
+
+	TEST_CASE("Scenario test") {
+		Engine engine("data/Demo");
+		auto& sc1 = engine.GetScenario();
+		{
+			Scenario sc2(sc1);
+		}
+		CHECK(sc1.GetIncomingTrains().front()->GetShuntingUnit()->GetTrains().front()->GetType()!=nullptr);
+		auto st1 = engine.StartSession(sc1);
+		engine.EndSession(st1);
+		CHECK(sc1.GetIncomingTrains().front()->GetShuntingUnit()->GetTrains().front()->GetType()!=nullptr);
+		for(int i=0; i<5; i++) {
+			CAPTURE("Test " + to_string(i));
+			Scenario sc3(sc1);
+			auto st2 = engine.StartSession(sc3);
+			int counter = 0;
+			while(true) {
+				try{
+					list<const Action*> &actions = engine.GetActions(st2);
+					if(actions.size() == 0) break;
+					auto it = actions.begin();
+					advance(it, counter++ % actions.size());
+					auto a = *it;
+					engine.ApplyAction(st2, a);
+				} catch(ScenarioFailedException& e) { break; }
+			}
+			engine.EndSession(st2);
+			CHECK(sc1.GetIncomingTrains().front()->GetShuntingUnit()->GetTrains().front()->GetType()!=nullptr);
+		}
+	}
 }
