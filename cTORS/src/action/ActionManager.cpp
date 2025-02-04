bf79a7cc29e6768f7b702069032f1ac965f14e12
KRolander <kromesrolandgyula@gmail.com>
2025-02-04 09:35:42 +0100

    Restructuring

--- cTORS/src/action/ActionManager.cpp
+++ cTORS/src/action/ActionManager.cpp
@@ -1,57 +1,69 @@
 #include "Action.h"
 #include "BusinessRules.h"
 
-void ActionManager::AddGenerator(const string& name, const ActionGenerator* generator) {
-	if (config->IsGeneratorActive(name)) {
+void ActionManager::AddGenerator(const string &name, const ActionGenerator *generator)
+{
+	if (config->IsGeneratorActive(name))
+	{
 		generators.push_back(generator);
 		generatorMap[name] = generator;
 	}
 }
 
-void ActionManager::Generate(const State* state, list<const Action*>& out) const {
-	for (auto& generator: generators) {
+void ActionManager::Generate(const State *state, list<const Action *> &out) const
+{
+	for (auto &generator : generators)
+	{
 		auto size = out.size();
 		generator->Generate(state, out);
-		//Filter new Actions
-		out.erase(remove_if(next(out.begin(), size), out.end(), [this, state](const Action*& a) {
+		// Filter new Actions
+		out.erase(remove_if(next(out.begin(), size), out.end(), 
+		
+		[this, state](const Action *&a){
 			auto result = IsValid(state, a);
 			if (!result.first) {
 				debug_out("Invalid action: " + a->toString() + " - " + result.second);
 				delete a;
 				return true;
 			}
-			return false;
-		}), out.end());
+			return false; 
+		}),
+		out.end());
 	}
 }
 
-pair<bool, string> ActionManager::IsValid(const State* state, const Action* action) const {
-	for (auto rule : validators) {
+pair<bool, string> ActionManager::IsValid(const State *state, const Action *action) const
+{
+	for (auto rule : validators)
+	{
 		auto result = rule->IsValid(state, action);
 		if (!result.first)
 			return result;
 	}
-	return make_pair(true,"");
+	return make_pair(true, "");
 }
 
 #ifndef ADD_GENERATOR
-#define ADD_GENERATOR(name, generator)\
-AddGenerator(name, new generator(config->GetActionParameters(name), location));
+#define ADD_GENERATOR(name, generator) \
+	AddGenerator(name, new generator(config->GetActionParameters(name), location));
 #endif
 
 #ifndef ADD_VALIDATOR
-#define ADD_VALIDATOR(validator) \
-if (config->IsBusinessRuleActive(#validator)) { \
-	validators.push_back(new validator(config)); \
-} 
+#define ADD_VALIDATOR(validator)                     \
+	if (config->IsBusinessRuleActive(#validator))    \
+	{                                                \
+		validators.push_back(new validator(config)); \
+	}
 #endif
 
-ActionManager::~ActionManager() {
+ActionManager::~ActionManager()
+{
 	DELETE_VECTOR(validators);
 	DELETE_VECTOR(generators);
 }
 
-void ActionManager::AddGenerators() {
+void ActionManager::AddGenerators()
+{
 	ADD_GENERATOR("move", MoveActionGenerator);
 	ADD_GENERATOR("move_helper", MoveHelperGenerator);
 	ADD_GENERATOR("arrive", ArriveActionGenerator);
@@ -63,45 +75,46 @@ void ActionManager::AddGenerators() {
 	ADD_GENERATOR("combine", CombineActionGenerator);
 }
 
-void ActionManager::AddValidators() {
-	//Arrival and departure
+void ActionManager::AddValidators()
+{
+	// Arrival and departure
 	ADD_VALIDATOR(in_correct_time_rule)
 	ADD_VALIDATOR(out_correct_time_rule)
 	ADD_VALIDATOR(out_correct_order_rule)
 
-	//Track occupation
+	// Track occupation
 	ADD_VALIDATOR(single_move_track_rule)
 	ADD_VALIDATOR(length_track_rule)
 	ADD_VALIDATOR(blocked_track_rule)
 	ADD_VALIDATOR(blocked_first_track_rule)
 
-	//Parking
+	// Parking
 	ADD_VALIDATOR(electric_track_rule)
 	ADD_VALIDATOR(legal_on_parking_track_rule)
 	ADD_VALIDATOR(legal_on_setback_track_rule)
 
-	//Shunting
+	// Shunting
 	ADD_VALIDATOR(electric_move_rule)
 	ADD_VALIDATOR(setback_once_rule)
 	ADD_VALIDATOR(setback_track_rule)
 
-	//Service tasks
+	// Service tasks
 	ADD_VALIDATOR(correct_facility_rule)
 	ADD_VALIDATOR(mandatory_service_task_rule)
 	ADD_VALIDATOR(optional_service_task_rule)
 	ADD_VALIDATOR(understaffed_rule)
 
-	//Facility
+	// Facility
 	ADD_VALIDATOR(available_facility_rule)
 	ADD_VALIDATOR(capacity_facility_rule)
 	ADD_VALIDATOR(disabled_facility_rule)
 
-	//Employee
+	// Employee
 
-	//Combine and Split
+	// Combine and Split
 	ADD_VALIDATOR(order_preserve_rule)
 	ADD_VALIDATOR(park_combine_split_rule)
 	ADD_VALIDATOR(setback_combine_split_rule)
 
-	//Matching
+	// Matching
 }
