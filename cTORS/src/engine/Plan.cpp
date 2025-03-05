d801c54ab89b97239acba6511074c0bcb7c07c1d
KRolander <kromesrolandgyula@gmail.com>
2025-03-05 12:06:51 +0100

    Add CreateRunResult() - new version to create it from HIP plan, CreateBeginMoveAction(), CreateEndMoveAction(), and helper fuctions to sort Vector of actions

--- cTORS/src/engine/Plan.cpp
+++ cTORS/src/engine/Plan.cpp
@@ -113,16 +113,15 @@ POSAction POSAction::CreatePOSAction(const Location *location, const Scenario *s
             {
                 // TODO how is the split action defined in protobuf? Current implementation: store the IDs of the first train.
                 vector<int> firstTrainIDs = GetTrainIDs(pb_action.task().trainunitids());
-                
+
                 // vector<int> copy_firstTrainIDs(firstTrainIDs.end()-1, firstTrainIDs.begin());
-                
+
                 // int index = 0;
                 // for(auto id : copy_firstTrainIDs)
                 // {
                 //     firstTrainIDs[index] = id;
                 //     index++;
                 // }
-
 
                 int splitIndex = 1;
                 for (auto &id : firstTrainIDs)
@@ -377,6 +376,340 @@ RunResult *RunResult::CreateRunResult(const Location *location, const PBRun &pb_
     return new RunResult(location->GetLocationFilePath(), scenario, plan, feasible);
 }
 
+PBAction RunResult::CreateBeginMoveAction(PB_HIP_Action &pb_hip_action)
+{
+    PBAction PBaction;
+
+    PBaction.set_suggestedstartingtime(pb_hip_action.starttime());
+    PBaction.set_suggestedfinishingtime(pb_hip_action.starttime());
+
+    PB_HIP_ShuntingUnit pb_shuntingUnit = pb_hip_action.shuntingunit();
+
+    for (auto &trainUnit : pb_shuntingUnit.members())
+    {
+        PBaction.add_trainunitids(trainUnit.id());
+    }
+
+    PBTaskAction *task_action = PBaction.mutable_task();
+
+    PBTaskType *taskType = task_action->mutable_type();
+
+    taskType->set_predefined(PBPredefinedTaskType::BeginMove);
+
+    return PBaction;
+}
+
+PBAction RunResult::CreateEndMoveAction(PB_HIP_Action &pb_hip_action)
+{
+    PBAction PBaction;
+
+    PBaction.set_suggestedstartingtime(pb_hip_action.endtime());
+    PBaction.set_suggestedfinishingtime(pb_hip_action.endtime());
+
+    PB_HIP_ShuntingUnit pb_shuntingUnit = pb_hip_action.shuntingunit();
+
+    for (auto &trainUnit : pb_shuntingUnit.members())
+    {
+        PBaction.add_trainunitids(trainUnit.id());
+    }
+
+    PBTaskAction *task_action = PBaction.mutable_task();
+
+    PBTaskType *taskType = task_action->mutable_type();
+
+    taskType->set_predefined(PBPredefinedTaskType::EndMove);
+
+    return PBaction;
+}
+
+
+// Compares two neighbooring action by their start time and duration
+// if result is positive than pb_action1 should be placed after pb_action2 in the 
+// actions vector
+int compare_helperfunction(PBAction pb_action1, PBAction pb_action2)
+{
+    int start1 = pb_action1.suggestedstartingtime();
+    int start2 = pb_action2.suggestedstartingtime();
+
+    int finish1 = pb_action1.suggestedfinishingtime();
+    int finish2 = pb_action2.suggestedfinishingtime();
+
+    // Compute durations
+    int duration1 = finish1 - start1;
+    int duration2 = finish2 - start2;
+
+    // Primary sort: suggestedStartingTime
+    if (start1 != start2)
+        return start1 - start2;
+
+    // Secondary sort: duration (if same starting time)
+    return duration1 - duration2;
+}
+
+// The actions vector is sorted by the start time and the duration of the actions
+// in a Increasing order, Arrive actions have to be before the BeginMove
+void sort_actions_helperfunction(vector<PBAction> &actions)
+{
+    int n = actions.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            int compare_res = compare_helperfunction(actions[j], actions[j + 1]);
+            if (compare_res > 0)
+            {
+                swap(actions[j], actions[j + 1]);
+            }
+            else if (compare_res == 0)
+            {
+                if (actions[j + 1].task().type().predefined() == PBPredefinedTaskType::Arrive)
+                    swap(actions[j], actions[j + 1]);
+            }
+        }
+    }
+}
+
+RunResult *RunResult::CreateRunResult(const PB_HIP_Plan &pb_hip_plan)
+{
+    PBRun pb_run;
+
+    PBPOSPlan pb_plan = pb_run.plan();
+
+    vector<PBAction> pb_actions;
+
+    vector<PBAction> pb_split_actions;
+
+    vector<PBAction> pb_combine_actions;
+
+    vector<PBAction> pb_combne_actions;
+
+    vector<PB_HIP_Action> pb_action(pb_hip_plan.actions().begin(), pb_hip_plan.actions().end());
+
+    int index = 0;
+    // Iterate over all the HIP action
+    for (PB_HIP_Action &hip_action : pb_action)
+    {
+
+        PBAction action_;
+
+        action_.set_suggestedstartingtime(hip_action.starttime());
+        action_.set_suggestedfinishingtime(hip_action.endtime());
+
+        if (hip_action.endtime() - hip_action.starttime() != 0)
+            action_.set_minimumduration(hip_action.endtime() - hip_action.starttime());
+
+        PB_HIP_ShuntingUnit hip_shuntingUnit = hip_action.shuntingunit();
+
+        for (auto &trainUnit : hip_shuntingUnit.members())
+        {
+            action_.add_trainunitids(trainUnit.id());
+        }
+
+        PB_HIP_TaskType taskType = hip_action.tasktype();
+
+        // test if predefined or other field is defined
+        if (taskType.has_predefined())
+        {
+
+            switch (taskType.predefined())
+            {
+            case PB_HIP_PredefinedTaskType::Move:
+            {
+                cout << "Move action" << std::endl;
+                PBMovementAction *move_action = action_.mutable_movement();
+
+                move_action->add_path(hip_action.location());
+
+                for (PB_HIP_Recource resource : hip_action.resources())
+                {
+                    move_action->add_path(resource.trackpartid());
+                }
+
+                PBAction BeginMoveAction = RunResult::CreateBeginMoveAction(hip_action);
+
+                pb_actions.push_back(BeginMoveAction);
+
+                pb_actions.push_back(action_);
+
+                // If next element is an Exit, the EndAction is not added
+
+                PB_HIP_Action next_action = pb_action[index + 1];
+
+                if (next_action.tasktype().predefined() != PB_HIP_PredefinedTaskType::Exit)
+                {
+                    PBAction EndMoveAction = RunResult::CreateEndMoveAction(hip_action);
+
+                    pb_actions.push_back(EndMoveAction);
+                }
+                break;
+            }
+            case PB_HIP_PredefinedTaskType::Split:
+            {
+                PBTaskAction *task_action = action_.mutable_task();
+                PBTaskType *taskType = task_action->mutable_type();
+
+                taskType->set_predefined(PBPredefinedTaskType::Split);
+
+                auto &trainUnits = hip_shuntingUnit.members();
+
+                task_action->add_trainunitids(trainUnits[0].id());
+
+                cout << "Split action" << std::endl;
+
+                pb_actions.push_back(action_);
+
+                break;
+            }
+            case PB_HIP_PredefinedTaskType::Combine:
+            {
+                PBTaskAction *task_action = action_.mutable_task();
+                PBTaskType *taskType = task_action->mutable_type();
+
+                taskType->set_predefined(PBPredefinedTaskType::Combine);
+
+                for (auto &trainUnit : hip_shuntingUnit.members())
+                {
+                    task_action->add_trainunitids(trainUnit.id());
+                }
+
+                pb_combne_actions.push_back(action_);
+
+                cout << "Combine action" << std::endl;
+                break;
+            }
+
+            case PB_HIP_PredefinedTaskType::Wait:
+            {
+                cout << "Wait action" << std::endl;
+                action_.mutable_break_();
+                pb_actions.push_back(action_);
+
+                break;
+            }
+            case PB_HIP_PredefinedTaskType::Arrive:
+            {
+                PBTaskAction *task_action = action_.mutable_task();
+
+                PBTaskType *taskType = task_action->mutable_type();
+
+                taskType->set_predefined(PBPredefinedTaskType::Arrive);
+
+                pb_actions.push_back(action_);
+
+                cout << "Arrive action" << std::endl;
+                break;
+            }
+            case PB_HIP_PredefinedTaskType::Exit:
+            {
+                PBTaskAction *task_action = action_.mutable_task();
+
+                PBTaskType *taskType = task_action->mutable_type();
+
+                taskType->set_predefined(PBPredefinedTaskType::Exit);
+
+                pb_actions.push_back(action_);
+                cout << "Exit action" << std::endl;
+                break;
+            }
+
+            default:
+                break;
+            }
+        }
+        else if (taskType.has_other())
+        {
+            PBTaskAction *task_action = action_.mutable_task();
+
+            PBTaskType *taskType_ = task_action->mutable_type();
+
+            taskType_->set_other(taskType.other());
+
+            task_action->set_location(hip_action.location());
+
+            for (auto &trainUnit : hip_shuntingUnit.members())
+            {
+                task_action->add_trainunitids(trainUnit.id());
+            }
+
+            for (PB_HIP_Recource resource : hip_action.resources())
+            {
+                PBFacilityInstance *facilites = task_action->add_facilities();
+                facilites->set_id(resource.facilityid());
+            }
+
+            pb_actions.push_back(action_);
+        }
+
+        string jsonResult;
+        google::protobuf::util::Status status = google::protobuf::util::MessageToJsonString(action_, &jsonResult);
+        if (status.ok())
+        {
+            std::cout << "JSON string: " << jsonResult << std::endl;
+        }
+        else
+        {
+            std::cerr << "Failed to convert protobuf to JSON: " << status.ToString() << std::endl;
+        }
+        index++;
+    }
+
+    // post-process combine actions
+    std::map<int, std::vector<PBAction>> startTimeToCombineActions;
+
+    for (auto &combine_action : pb_combne_actions)
+    {
+        startTimeToCombineActions[combine_action.suggestedstartingtime()].push_back(combine_action);
+    }
+
+    for (auto &[startTime, combine_actions] : startTimeToCombineActions)
+    {
+
+        PBAction PBaction = combine_actions[0];
+        // Remove the first train unit id - the one which is the first unit
+        //to be combined with the rest
+        PBaction.mutable_trainunitids()->Clear();
+
+        PBTaskAction *task_action = PBaction.mutable_task();
+
+        for (size_t i = 1; i < combine_actions.size(); i++)
+        {
+            // Add all the train units which must be combined
+            // a combine action might contains multiple units
+            PBAction PBaction_other = combine_actions[i];
+            auto &trainunits = PBaction_other.trainunitids();
+
+            for (auto &unit : trainunits){
+                task_action->add_trainunitids(unit);
+                PBaction.add_trainunitids(unit);
+            }
+        }
+        pb_actions.push_back(PBaction);
+    }
+
+    // Sort actions by start time and duration
+    sort_actions_helperfunction(pb_actions);
+
+    // Add actions to plan 
+    for (auto &action : pb_actions)
+    {
+        PBAction *action_ = pb_plan.add_actions();
+        action_->CopyFrom(action);
+        // *pb_plan.add_actions() = action;
+    }
+
+    string jsonResult;
+    google::protobuf::util::Status status = google::protobuf::util::MessageToJsonString(pb_plan, &jsonResult);
+    if (status.ok())
+    {
+        std::cout << "****JSON string: " << jsonResult << std::endl;
+    }
+    else
+    {
+        std::cerr << "Failed to convert protobuf to JSON: " << status.ToString() << std::endl;
+    }
+    return nullptr;
+}
+
 void GetRunResultProto(string planFileString, PBRun &pb_runResult)
 {
     // PBRun pb_runResult2;
@@ -390,5 +723,20 @@ void GetRunResultProto(string planFileString, PBRun &pb_runResult)
     else
     {
         std::cout << "RunResult Protobuf has been initialized and may contain data." << std::endl;
+    }
+}
+
+void ParseHIP_PlanFromJson(string planFileString, PB_HIP_Plan &pb_hip_plan)
+{
+
+    parse_json_to_pb(fs::path(planFileString), &pb_hip_plan);
+
+    if (!pb_hip_plan.IsInitialized() || pb_hip_plan.ByteSizeLong() == 0)
+    {
+        std::cerr << "HIP plan Protobuf is empty or not initialized." << std::endl;
+    }
+    else
+    {
+        std::cout << "HIP plan Protobuf has been initialized and may contain data." << std::endl;
     }
 }
