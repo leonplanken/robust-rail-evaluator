3cb0d6d04fa559dbce5f0bcdde370566919cbd55
KRolander <kromesrolandgyula@gmail.com>
2025-04-03 14:20:24 +0200

    Add run example for testing plan evaluation - bash contains some relative path

---
+++ run_eval_example.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+./build/TORS --mode "EVAL" --path_location "./data/Demo/TUSS-Instance-Generator/kleine_brinkhorst_v2" --path_scenario "./data/Demo/TUSS-Instance-Generator/kleine_brinkhorst_v2/scenario.json" --path_plan "./data/Demo/TUSS-Instance-Generator/kleine_brinkhorst_v2/plan.json" --plan_type HIP
