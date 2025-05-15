cc41b89deb91830bf2d0caba58be52cc5be69d26
KRolander <kromesrolandgyula@gmail.com>
2025-05-15 14:52:25 +0200

    Add run examplethat allows to store the evaluator's results.

---
+++ run_eval_and_store_example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+./build/TORS --mode "EVAL_AND_STORE" \
+    --path_location "./data/Demo/TUSS-Instance-Generator/kleine_brinkhorst_v2/Fixed" \
+    --path_scenario "./data/Demo/TUSS-Instance-Generator/kleine_brinkhorst_v2/Fixed/scenario.json" \
+    --path_plan "./data/Demo/TUSS-Instance-Generator/kleine_brinkhorst_v2/Fixed/plan.json" \
+    --path_eval_result "./data/Demo/TUSS-Instance-Generator/kleine_brinkhorst_v2/Fixed/evaluation_results.txt" \
+    --plan_type "HIP"
