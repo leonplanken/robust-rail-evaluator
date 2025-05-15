b905d12735873a1a9600cb29aab6b21ba3d02275
KRolander <kromesrolandgyula@gmail.com>
2025-05-15 15:50:13 +0200

    Remove unused data, add new data to run example

--- run_eval_example.sh
+++ run_eval_example.sh
@@ -2,7 +2,7 @@
 
 
 ./build/TORS --mode "EVAL" \
-    --path_location "./data/Demo/TUSS-Instance-Generator/kleine_brinkhorst_v2/Fixed" \
-    --path_scenario "./data/Demo/TUSS-Instance-Generator/kleine_brinkhorst_v2/Fixed/scenario.json" \
-    --path_plan "./data/Demo/TUSS-Instance-Generator/kleine_brinkhorst_v2/Fixed/plan.json" \
+    --path_location "./data/Demo/TUSS-Instance-Generator/kleine_binckhorst" \
+    --path_scenario "./data/Demo/TUSS-Instance-Generator/kleine_binckhorst/scenario.json" \
+    --path_plan "./data/Demo/TUSS-Instance-Generator/kleine_binckhorst/plan.json" \
     --plan_type "Solver"
